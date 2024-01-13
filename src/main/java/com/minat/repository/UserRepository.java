6b168db42efbbf953769a7162c20e4fab5f84565
minatisleeping <minatt2002@gmail.com>
2024-01-13 12:28:41 +0700

    Play w service layer to call the repo player & test until work

--- src/main/java/com/minat/repository/UserRepository.java
+++ src/main/java/com/minat/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.minat.repository;
 
-import com.minat.model.ApplicationUser;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import com.minat.model.ApplicationUser;
 
 @Repository
 public interface UserRepository extends JpaRepository<ApplicationUser, Integer> {
--- src/main/java/com/minat/service/UserService.java
+++ src/main/java/com/minat/service/UserService.java
@@ -1,7 +1,6 @@
 package com.minat.service;
 
-import com.minat.model.Role;
-import com.unkownkoder.models.ApplicationUser;
+import com.minat.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,8 +8,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Set;
+
 
 @Service
 public class UserService implements UserDetailsService {
@@ -18,16 +16,14 @@ public class UserService implements UserDetailsService {
     @Autowired
     private PasswordEncoder encoder;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        System.out.println("In the user detail service");
+        System.out.println("In the user details service");
 
-        if (!username.equals("minat")) throw new UsernameNotFoundException("Not minat!");
-
-        Set<Role> roles = new HashSet<>();
-        roles.add(new Role(1, "USER"));
-
-        return new ApplicationUser(1, "minat", encoder.encode("password"), roles);
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User is not valid"));
     }
 }
